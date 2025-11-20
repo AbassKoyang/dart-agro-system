68a93bc9779f103d59636203faf6f1d232b6d1e9
Abass Koyang Ahmad <abasskoyang12345@gmail.com>
2025-11-20 12:27:53 +0100

    complete home page

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -44,7 +44,7 @@ const Footer = () => {
                 <path d="M18 12C18 13.5913 17.3679 15.1174 16.2426 16.2426C15.1174 17.3679 13.5913 18 12 18C10.4087 18 8.88258 17.3679 7.75736 16.2426C6.63214 15.1174 6 13.5913 6 12C6 10.4087 6.63214 8.88258 7.75736 7.75736C8.88258 6.63214 10.4087 6 12 6C13.5913 6 15.1174 6.63214 16.2426 7.75736C17.3679 8.88258 18 10.4087 18 12Z" stroke="black" stroke-width="2"/>
                 <path d="M13 12C13 12.2652 12.8946 12.5196 12.7071 12.7071C12.5196 12.8946 12.2652 13 12 13C11.7348 13 11.4804 12.8946 11.2929 12.7071C11.1054 12.5196 11 12.2652 11 12C11 11.7348 11.1054 11.4804 11.2929 11.2929C11.4804 11.1054 11.7348 11 12 11C12.2652 11 12.5196 11.1054 12.7071 11.2929C12.8946 11.4804 13 11.7348 13 12Z" stroke="black" stroke-width="2"/>
             </svg>
-            <h1 className="text-4xl lg:text-[140px] text-black font-dm-mono font-medium lg:leading-[70px] translate-y-[45px] lg:translate-y-[87%]">DartAgro System</h1>
+            <h1 className="text-3xl lg:text-[140px] text-black font-dm-mono font-medium lg:leading-[70px] translate-y-[45px] lg:translate-y-[87%]">DartAgro System</h1>
         </div>
     </footer>
   )
--- components/home/BlogPreview.tsx
+++ components/home/BlogPreview.tsx
@@ -4,8 +4,8 @@ import React from 'react'
 const BlogPreview = ({title, date, text, imageUrl}:{title: string; text: string; date: string; imageUrl: string}) => {
   return (
     <Link href='#' className='w-full lg:h-[392px] flex flex-col lg:flex-row justify-between'>
-        <div className="lg:h-full flex flex-col lg:flex-row items-start gap-5">
-            <div className="w-[518px] h-[392px] lg:h-full overflow-hidden">
+        <div className="w-full lg:w-fit lg:h-full flex flex-col lg:flex-row items-start gap-5">
+            <div className="w-full lg:w-[518px] h-[392px] lg:h-full overflow-hidden">
                 <img src={imageUrl} alt="Blog preview image" className='size-full object-center object-cover' />
             </div>
             <div className="lg:h-full flex flex-col justify-between">
